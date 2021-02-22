03a7c038eee5579a3fcc31fee34f23df3f6308a8
binaydhakal <45663897+binaydhakal@users.noreply.github.com>
2021-02-22 21:05:40 +0545

    Update Bank1.sol

--- contracts/Bank1.sol
+++ contracts/Bank1.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
 
 contract Bank1 {
     
@@ -10,10 +11,10 @@ contract Bank1 {
     }
     
     struct Kyc{
-        string fullname;
-        string fathername;
-        string gender;
-        string dob;
+        string[5] familyinfo;
+        string[3] personaladd;
+        string[3] personalinfo;
+        string[5] identityinfo;
     }
     
     
@@ -38,21 +39,12 @@ contract Bank1 {
     address[] public contracts;
         
     Status public status;
+
+    
    
     mapping(address => User) public userDetails;
     
-    mapping(address => Kyc ) public kycDetails;
-    
-    
-    //     constructor() public {
-    
-    //     address myconnectionaddress = 0xd9145CCE52D386f254917e481eB44e9943F39138;
-    //     myconnection = Connection(myconnectionaddress);
-        
-    // }
-    
-    
-    
+    mapping(address => Kyc ) private kycDetails;
     
     
     function random() private view returns (uint8) {
@@ -83,24 +75,27 @@ contract Bank1 {
         
     }
     
-
     
-    function buildKyc(string memory _fullname, string memory _fathername, string memory _gender, string memory _dob) public {
+    function buildKyc(string[5] memory fi, string[3] memory pa, string[3] memory pi, string[5] memory ii) public {
          // Section where we store the kyc of the customer in blockchain
-         kycDetails[msg.sender] = Kyc(_fullname,_fathername,_gender,_dob);
+
+        
+         kycDetails[msg.sender] = Kyc(fi,pa,pi,ii);
+        
              
     } 
      
     function KycBlock() public payable{
         
-        KYC kyc = new KYC(kycDetails[msg.sender].fullname, kycDetails[msg.sender].fathername, kycDetails[msg.sender].gender, kycDetails[msg.sender].dob);
+        
+        KYC kyc = new KYC(kycDetails[msg.sender].familyinfo,kycDetails[msg.sender].personaladd,kycDetails[msg.sender].personalinfo,kycDetails[msg.sender].identityinfo);
         
         lastContractAddress = address(kyc);
         emit NewKycAddress(lastContractAddress);
         
     } 
      
-    function GetKyc(address _youraddress) public view returns(string memory _fullname, string memory _fathername, string memory _dob, string memory _gender){
+    function GetKyc(address _youraddress) public view returns(string[5] memory, string[3] memory, string[3] memory, string[5] memory){
         
         return KYC(_youraddress).giveKyc();
     }
@@ -115,26 +110,27 @@ contract KYC {
     
         
     struct Kyc{
-        string fullname;
-        string fathername;
-        string gender;
-        string dob;
+        string[5] _familyinfo;
+        string[3] _personaladd;
+        string[3] _personalinfo;
+        string[5] _identityinfo;
     }
     
-    mapping( uint => Kyc ) public newKyc;
+    mapping( uint => Kyc ) private newKyc;
     
-    constructor(string memory _fullname, string memory _fathername, string memory _gender, string memory _dob) public payable {
-        newKyc[1].fullname = _fullname;
-        newKyc[1].fathername = _fathername;
-        newKyc[1].gender = _gender;
-        newKyc[1].dob = _dob;
+    constructor(string[5] memory fai, string[3] memory padd, string[3] memory pin, string[5] memory iinf) public payable {
+        newKyc[1]._familyinfo = fai;
+        newKyc[1]._personaladd = padd;
+        newKyc[1]._personalinfo = pin;
+        newKyc[1]._identityinfo = iinf;
         
     }
     
-    function giveKyc() public view returns(string memory _fullname, string memory _fathername, string memory _dob, string memory _gender){
-        
-        return(newKyc[1].fullname, newKyc[1].fathername, newKyc[1].gender, newKyc[1].dob);
+    function giveKyc() public view returns(string[5] memory, string[3] memory, string[3] memory, string[5] memory){
+
+        return(newKyc[1]._familyinfo,newKyc[1]._personaladd,newKyc[1]._personalinfo,newKyc[1]._identityinfo);
     }
     
 }
+
 
